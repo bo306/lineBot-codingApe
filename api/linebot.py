3e26b0c132c386fcacf13aad2cdea9136141b8c3
bo306 <123080633+bo306@users.noreply.github.com>
2024-10-11 14:39:42 +0800

    Update linebot.py

--- api/linebot.py
+++ api/linebot.py
@@ -1,11 +1,10 @@
 from flask import Flask, request, abort
 from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
-from linebot.models import (MessageEvent, TextMessage, TextSendMessage, TemplateSendMessage, CarouselColumn,
-                            CarouselTemplate, MessageAction, URIAction, ImageCarouselColumn, ImageCarouselTemplate,
-                            ImageSendMessage)
+from linebot.models import (MessageEvent, TextMessage, TextSendMessage)
 import os
-from jinja2.utils import escape
+import html  # 使用標準庫的 escape 函數
+
 line_bot_api = LineBotApi(os.getenv("LINE_CHANNEL_ACCESS_TOKEN"))
 line_handler = WebhookHandler(os.getenv("LINE_CHANNEL_SECRET"))
 
@@ -17,24 +16,22 @@ def home():
 
 @app.route("/webhook", methods=['POST'])
 def callback():
-    # get X-Line-Signature header value
     signature = request.headers['X-Line-Signature']
-    # get request body as text
     body = request.get_data(as_text=True)
     app.logger.info("Request body: " + body)
-    # handle webhook body
+
     try:
         line_handler.handle(body, signature)
     except InvalidSignatureError:
         abort(400)
     return 'OK'
 
-
 @line_handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
+    escaped_message = html.escape(event.message.text)  # 使用 html.escape
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text=event.message.text))
+        TextSendMessage(text=escaped_message))
 
 if __name__ == "__main__":
     app.run()
